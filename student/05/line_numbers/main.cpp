8fc91a17f95a5b787328bf82ea970ea11644b36c
Enna Augustin <enna.augustin@tuni.fi>
2023-10-05 14:10:34 +0300

    eka

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string input = "";
+    string output = "";
+
+    cout << "Input file: ";
+    cin >> input;
+
+    cout<< "Output file: ";
+    cin >> output;
+
+    ifstream input_olio(input);
+
+    if ( not input_olio ) {
+        cout << "Error! The file not_a_file.input cannot be opened." << endl;
+        return EXIT_FAILURE;
+    }
+    else {
+        ofstream output_olio(output);
+        int x=1;
+        string line;
+
+        while(getline(input_olio, line)){
+            output_olio << x <<" "<<line<<endl;
+            x++;
+        }
+
+        input_olio.close();
+        output_olio.close();
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_SUCCESS;
+}
