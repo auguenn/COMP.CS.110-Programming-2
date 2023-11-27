59add5a39338b671472343ab2b5255e621d965dd
Enna Augustin <enna.augustin@tuni.fi>
2023-11-27 18:02:54 +0200

    eka

---
+++ student/11/bmi/mainwindow.cpp
@@ -0,0 +1,41 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->closeButton,SIGNAL(clicked()),this,SLOT(close()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_countButton_clicked()
+{
+    int weight = std::stoi(ui->weightLineEdit->text().toStdString());
+    int height = std::stoi(ui->heightLineEdit->text().toStdString());
+    if (height == 0) {
+        ui->resultLabel->setText(QString("Cannot count."));
+        return;
+    }
+    double result = weight/((height/100.0)*(height/100.0));
+
+    ui->resultLabel->setNum(result);
+
+    if (result < 18.5)
+    {
+        ui->infoTextBrowser->setText(QString("You are underweight."));
+    }
+    else if (result > 25.0)
+    {
+        ui->infoTextBrowser->setText(QString("You are overweight."));
+    }
+    else
+    {
+        ui->infoTextBrowser->setText(QString("Your weight is normal."));
+    }
+}
